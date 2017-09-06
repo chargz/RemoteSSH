bf711803a8f6bc935b21308fe67971fd1788b6bf
chargz <mikerobkop@gmail.com>
2017-09-06 12:34:07 +0530

    Update starter.sh

--- starter.sh
+++ starter.sh
@@ -4,5 +4,5 @@ killall ngrok
 gnome-terminal -x sh -c "./ngrok tcp 22; bash"
 sleep 15
 curl http://localhost:4040/api/tunnels > tunnels.json
-curl -X POST -H 'Content-type: application/json' --data '{"text":"Here is your URL - '$(python ./parsr.py)' "}' [INSERT SLACK URL]
+curl -X POST -H 'Content-type: application/json' --data '{"text":"Here is your URL - '$(python ./parsr.py)' "}' <INSERT SLACK URL>
 rm tunnels.json
