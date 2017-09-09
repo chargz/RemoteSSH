28813ab84700a06c65db9815e6987e221cdf28f8
chargz <mikerobkop@gmail.com>
2017-09-09 18:23:54 +0530

    Update starter.sh

--- starter.sh
+++ starter.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
 killall ngrok
-gnome-terminal -x sh -c "./ngrok tcp 22; bash"
+export DISPLAY=:0.0
+gnome-terminal -e "bash -c \"~/remotessh/ngrok tcp 22; exec bash\""
 sleep 15
 curl http://localhost:4040/api/tunnels > tunnels.json
 curl -X POST -H 'Content-type: application/json' --data '{"text":"Here is your URL - '$(python ./parsr.py)' "}' <INSERT SLACK URL>
